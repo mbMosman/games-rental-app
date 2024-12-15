3e18b564e751329edf15b8b66ad66572dad6e233
mbmosman <mbMosman@users.noreply.github.com>
2024-12-15 14:30:46 -0600

    search form functional

--- src/views/GameSearchView.jsx
+++ src/views/GameSearchView.jsx
@@ -3,21 +3,22 @@ import { GameService } from '../services/GameService';
 
 export default function GamesSearchView() {
 
-  const [searchString, setSearchString] = useState('Civilization');
+  const [searchString, setSearchString] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [pageNumber, setPageNumber] = useState(0);
   const [availablePages, setAvailablePages] = useState(0);
 
-  useEffect(() => {
-    search();
-  }, []);
+  function handleSearch() {
+    event.preventDefault();
+    search(searchString, pageNumber)
+  }
 
-  function search() {
+  function search(searchStr, page) {
     setIsLoading(true);
 
-    GameService.searchGames(searchString, pageNumber)
+    GameService.searchGames(searchStr, page)
       .then(response => {
         const data = response.data;
         setSearchResults(data.results);
@@ -38,14 +39,20 @@ export default function GamesSearchView() {
     setErrorMessage('Error performing search, please try again.');
   }
 
+  function handleAddToCart(game) {
+    console.log("TODO - add to cart: ", game.name);
+  }
+
   return (
     <div className="view">
       <h2 className="view-header">Select Games to Rent</h2>
       <form className="search-form">
         <div className="search-control">
-          <input className="search-input" id="search" type="text" autoComplete="off" />
-          <button type="reset">X</button>
-          <button type="submit">Search</button>
+          <input className="search-input" id="search" type="text" autoComplete="off"
+              onChange={(event) => setSearchString(event.target.value)} 
+              value={searchString} />
+          <button type="reset" onClick={() => setSearchString('')}>X</button>
+          <button type="submit" onClick={handleSearch}>Search</button>
         </div>
       </form>
       <div className="search-results">
@@ -61,7 +68,7 @@ export default function GamesSearchView() {
                       <div className="game-detail">
                         <header className="game-header">
                           <h3>{game.name}</h3>
-                          <button className="btn-rent-game">Add to cart</button>
+                          <button className="btn-rent-game" onClick={(event) => handleAddToCart(game)}>Add to cart</button>
                         </header>
                         <p>{game.deck}</p>
                       </div>
